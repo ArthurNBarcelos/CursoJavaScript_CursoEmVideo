75176256947ee6578eb339e53bc4652f0ae6f84b
Arthur N. Barcelos <arthurneutzlingbarcelos@gmail.com>
2025-01-25 12:26:16 -0300

    exercicios

---
+++ Aula 14 (exercícios práticos)/exercicio_016/scripts.js
@@ -0,0 +1,11 @@
+/*
+    Um site para fazer uma contagem de X número até Y número e pular de Z em Z.
+
+    Vai aparecer a contagem no site, exemplo:
+    De: 1
+    Até: 10
+    Pulando: 2
+    Restultado: 1, 3, 5, 7, 9, FIM
+
+    Impossível pular de 0 em 0, então, será usado 1 (1, 2, 3...)
+*/
---
+++ Aula 14 (exercícios práticos)/exercicio_017/scripts.js
@@ -0,0 +1,18 @@
+/*
+    Fazer um site que calcule a tabuada de um número X, de 1 até 10
+
+    Exemplo:
+    Número: 10
+     10 x 1 = 10
+     10 x 2 = 20
+     10 x 3 = 30
+     10 x 4 = 40
+     10 x 5 = 50
+     10 x 6 = 60
+     10 x 7 = 70
+     10 x 8 = 80
+     10 x 9 = 90
+     10 x 10 = 100
+
+    Utilizar um "select", uma lista em HTML onde ficará a tabuada
+*/
