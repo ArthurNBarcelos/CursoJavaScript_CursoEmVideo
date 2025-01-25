834ac35839f499e141b016b19ab15e1508cc32e0
Arthur N. Barcelos <arthurneutzlingbarcelos@gmail.com>
2025-01-25 14:52:41 -0300

    exercicio contador

--- Aula 14 (exercícios práticos)/exercicio_016/scripts.js
+++ Aula 14 (exercícios práticos)/exercicio_016/scripts.js
@@ -9,3 +9,49 @@
 
     Impossível pular de 0 em 0, então, será usado 1 (1, 2, 3...)
 */
+
+//Salvar as entradas do usuário
+var nInicial = document.getElementById("nInicial")
+var nFinal = document.getElementById("nFinal")
+var nInter = document.getElementById("nInter")
+
+function contar() {
+    var contagem = document.getElementById("contagem")
+    if(nInicial < nFinal) {
+        while(nInicial <= nFinal) {
+            console.log(nInicial)
+            nInicial = nInicial + nInter
+        }
+    } else {
+        while(nInicial >= nFinal) {
+            console.log(nInicial)
+            nInicial = nInicial - nInter
+        }
+    }
+}
+
+/*
+  nInicial = 0
+  nFinal = 10
+  nInter = 2
+
+  0,2,4,6,8,10
+*/
+
+/*
+var nInicial = 100
+var nFinal = 50
+var nInter = 1
+
+if (nInicial < nFinal) {
+    while (nInicial <= nFinal) {
+        console.log(nInicial)
+        nInicial = nInicial + nInter
+    }
+} else {
+    while (nInicial >= nFinal) {
+        console.log(nInicial)
+        nInicial = nInicial - nInter
+    }
+}
+*/
