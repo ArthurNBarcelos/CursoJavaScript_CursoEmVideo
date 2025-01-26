3b3e865e0f4f3c2d01413a24d854909bf053168b
Arthur N. Barcelos <arthurneutzlingbarcelos@gmail.com>
2025-01-26 14:16:52 -0300

    optimization

--- Aula 14 (exercícios práticos)/exercicio_016/script.js
+++ Aula 14 (exercícios práticos)/exercicio_016/script.js
@@ -3,7 +3,38 @@ function calcular() {
     var vF = Number(document.getElementById("vF").value)
     var vIn = Number(document.getElementById("vIn").value)
     var res = document.getElementById("res")
-    res.innerHTML = ``
+    res.innerHTML = `` //limpa o resultado anterior
+
+    if (vI != "" || vF != "" || vIn != "") {
+        if (vI != vF) {
+            if (vIn > 0) {
+                if (vI < vF) {
+                    //CRESCENTE
+                    for (var c = vI; c <= vF; c += vIn) {
+                        res.innerHTML += `${c} `
+                        res.style.padding = "20px"
+                    }
+                } else {
+                    //DECRESCENTE
+                    for (var c = vI ; c >= vF; c -= vIn) {
+                        res.innerHTML += `${c} `
+                        res.style.padding = "20px"
+                    }
+                }
+            } else {
+                alert("[ERRO] O Número de Intervalos precisa ser maior que zero!")
+            }
+        } else {
+            alert("[ERRO] Valor Inicial e Valor Final devem ser diferentes!")
+        }
+    } else {
+        alert("[ERRO] Por favor, preencha todos os campos!")
+    }
+
+}
+
+/*
+    Código antigo, 
 
     if (vI < vF) {
     if (vI != "" || vF != "" || vIn != "") {
@@ -41,4 +72,4 @@ function calcular() {
             alert("[ERRO] Por favor, preencha todos os campos!")
         }
     }
-}
+*/
