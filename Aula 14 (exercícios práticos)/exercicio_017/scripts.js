1a92c0b91882365b78799ebf490ced13cbf1ea99
Arthur N. Barcelos <arthurneutzlingbarcelos@gmail.com>
2025-01-30 13:28:03 -0300

    pratica_008

--- Aula 14 (exercícios práticos)/exercicio_017/scripts.js
+++ Aula 14 (exercícios práticos)/exercicio_017/scripts.js
@@ -1,3 +1,22 @@
+function calcular() {
+    let num = Number(document.getElementById('numero').value)
+    let tabuada = document.getElementById('tabuada')
+    if (num == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        tabuada.innerHTML = ''
+       let contador = 1
+       while (contador <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${contador} = ${num*contador}`
+            //item.value = `tabuada${contador}` //Serve para identificar o item selecionado, mas não utilizando o JavaScritp, mas talvez uma tecnologia de back-end
+            tabuada.appendChild(item)
+            contador++
+       }
+    }
+}
+
+
 /*
     Fazer um site que calcule a tabuada de um número X, de 1 até 10
 
---
+++ Aula 14 (exercícios práticos)/pratica_008/script.js
@@ -0,0 +1,11 @@
+function calcular() {
+    let nI = Number(document.getElementById('nI').value)
+    let nF = Number(document.getElementById('nF').value)
+    let nIn = Number(document.getElementById('nIn').value)
+
+
+}
+
+/*
+Contade de X a Y com Z intervalos
+*/
